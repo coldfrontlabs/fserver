4191c5b9b6dfafb7102ff1b6c3bbcbee83d5031c
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2013-04-08 20:43:47 -0400

    Added custom XML template header for releases view

---
+++ templates/views-data-export-xml-header--fserver-releases.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Describe the file
+ *
+ * @author Mathew Winstone <mwinstone@coldfrontlabs.ca>
+ * @copyright 2012 Coldfront Labs Inc.
+ * @license Copyright (c) 2011 All rights reserved
+ */
+?>
+<<?php print $root_node; ?>>
